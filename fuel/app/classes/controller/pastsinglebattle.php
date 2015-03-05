3adf80703a7940bd9c9ca3e0aa6794e5991fc628
TakeshiMatsumoto <take99matsu@yahoo.co.jp>
2015-03-05 21:01:20 +0900

    微修正

--- fuel/app/classes/controller/pastsinglebattle.php
+++ fuel/app/classes/controller/pastsinglebattle.php
@@ -6,21 +6,20 @@ class Controller_Pastsinglebattle extends Controller_Admin
 {
 	public function action_index(){
 		$total_page=DB::select()->from('singlebattlemaster')->where("battle_state","=", 3)->execute()->count();
-		//$base_url= Uri::base();
 		$config = array(
-	    'pagination_url' => '/Pastsinglebattle/',
-	    'total_items'    => $total_page,
-	    'per_page'       => 30,
-	    'uri_segment'    => 2,
+		    'pagination_url' => '/Pastsinglebattle/',
+		    'total_items'    => $total_page,
+		    'per_page'       => 30,
+		    'uri_segment'    => 2,
 		);
 		
 		$pagination = Pagination::forge('mypagination', $config);
 		$data['past_single_battle'] = DB::select()
                             ->from('singlebattlemaster')
-							->where("battle_state","=", 3)
+			　　->where("battle_state","=", 3)
                             ->limit($pagination->per_page)
                             ->offset($pagination->offset)
-							-> order_by('id','desc')
+			-> order_by('id','desc')
                             ->execute()
                             ->as_array();
 		$data['pagination'] = $pagination;
